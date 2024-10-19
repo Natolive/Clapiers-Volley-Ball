036403c17311f957b7a2ec1d011ada9af7ccb581
Nathan Olive <65222446+Natolive@users.noreply.github.com>
2024-10-19 16:49:04 +0200

    add some simple cards for dashboard

---
+++ app/Views/backend/dashboard.php
@@ -0,0 +1,29 @@
+<div class="row">
+    <div class="col s12 m6 l4">
+        <div class="card red lighten-2">
+            <div class="card-content white-text">
+                <span class="card-title">Nombres d'équipes</span>
+                <p>I am a very simple card. I am good at containing small bits of information.
+                    I am convenient because I require little markup to use effectively.</p>
+            </div>
+        </div>
+    </div>
+    <div class="col s12 m6 l4">
+        <div class="card red lighten-2">
+            <div class="card-content white-text">
+                <span class="card-title">Nombres de matchs de la semaines</span>
+                <p>I am a very simple card. I am good at containing small bits of information.
+                    I am convenient because I require little markup to use effectively.</p>
+            </div>
+        </div>
+    </div>
+    <div class="col s12 m6 l4">
+        <div class="card red lighten-2">
+            <div class="card-content white-text">
+                <span class="card-title">Nombres de licenciés</span>
+                <p>I am a very simple card. I am good at containing small bits of information.
+                    I am convenient because I require little markup to use effectively.</p>
+            </div>
+        </div>
+    </div>
+</div>
